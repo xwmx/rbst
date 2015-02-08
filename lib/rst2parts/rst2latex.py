d9a058641d1c15b8d5b14a957a63593fce3a6923
William Melody <hi@williammelody.com>
2015-02-08 14:20:07 -0800

    Use sys.stdout.write for rst2latex
    This is preferrable to print() since it doesn't do any additional
    formatting. This is also what is used for rst2html, so this change
    brings them to the same functionality.

--- lib/rst2parts/rst2latex.py
+++ lib/rst2parts/rst2latex.py
@@ -6,6 +6,7 @@ try:
 except:
     pass
 
+import sys
 from transform import transform
 from docutils.writers.latex2e import Writer
 
@@ -13,4 +14,4 @@ def main():
     return transform(writer=Writer(), part='whole')
 
 if __name__ == '__main__':
-    print(main())
+    sys.stdout.write(main())
