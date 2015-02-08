630c22d5f43c89cb1821144028acd5a4f620c1da
William Melody <hi@williammelody.com>
2015-02-08 10:49:03 -0800

    Add missing EOF newlines and remove unnecessary whitespace

--- lib/rbst.rb
+++ lib/rbst.rb
@@ -1,7 +1,7 @@
 # encoding: UTF-8
 
 class RbST
-  
+
   @@executable_path = File.expand_path(File.join(File.dirname(__FILE__), "rst2parts"))
   @@executables = {
     :html  => File.join(@@executable_path, "rst2html.py"),
@@ -12,17 +12,17 @@ class RbST
   def self.convert(*args)
     new(*args).convert
   end
-  
+
   # Print LaTeX-Specific Options, General Docutils Options and reStructuredText Parser Options.
   def self.latex_options
     new.print_options(:latex)
   end
-  
+
   # Print HTML-Specific Options, General Docutils Options and reStructuredText Parser Options.
   def self.html_options
     new.print_options(:html)
   end
-  
+
   # Specify custom executables to use instead of the default rst2latex.py and
   # rst2html.py scripts. Takes a hash with two possible keys, :html and :latex,
   # which should contain a full path to the alternative executable.
@@ -34,7 +34,7 @@ class RbST
   end
   # Return the executable hash.
   def self.executables; @@executables end
-  
+
   # Takes a string or file path plus any additional options and creates a new converter object.
   def initialize(*args)
     target = args.shift
@@ -47,21 +47,21 @@ class RbST
     execute "python #{@@executables[@output_format]}" + convert_options
   end
   alias_method :to_s, :convert
-  
+
   # Converts the object's input to HTML.
   def to_html(*args)
     @output_format = :html
     @options += args
     convert
   end
-  
+
   # Converts the object's input to LaTeX.
   def to_latex(*args)
     @output_format = :latex
     @options += args
     convert
   end
-  
+
   # Formats and prints the options from the docutils help in the way they'd be specified in RbST: strings, symbols and hashes.
   def print_options(format)
     help = execute("python #{@@executables[format]} --help")
@@ -77,13 +77,13 @@ class RbST
     help.gsub!(/(:\w) </, '\1 => <')
     puts help
   end
-  
+
 protected
-  
+
   def execute(command)
     output = ''
-    IO.popen(command, "w+") do |f| 
-      f.puts @target 
+    IO.popen(command, "w+") do |f|
+      f.puts @target
       f.close_write
       output = f.read
     end
@@ -99,7 +99,7 @@ protected
       end
     end
   end
-  
+
   def convert_opts_with_args(opt)
     opt.inject('') do |string, (flag, val)|
       flag = flag.to_s.gsub(/_/, '-')
--- test/test_rbst.rb
+++ test/test_rbst.rb
@@ -13,13 +13,13 @@ class TestRbST < Test::Unit::TestCase
     end
     @rst2parts_path = File.expand_path(File.join(File.dirname(__FILE__), '..', 'lib', 'rst2parts'))
   end
-  
+
   should "call bare rest2parts when passed no options" do
     converter = RbST.new(@rst_file)
     converter.expects(:execute).with("python #{@rst2parts_path}/rst2html.py").returns(true)
     assert converter.convert
   end
-  
+
   should "convert with custom executable" do
     executables = {:html => "/some/path/2html.py"}
     default_executables = RbST.executables
@@ -29,7 +29,7 @@ class TestRbST < Test::Unit::TestCase
     assert converter.to_html
     RbST.executables = default_executables
   end
-  
+
   should "raise error when passed bad executable key" do
     executables = {:markdown => "/some/path/2markdown.py"}
     begin
@@ -39,17 +39,17 @@ class TestRbST < Test::Unit::TestCase
       assert true
     end
   end
-  
+
   should "convert ReST to html" do
     html = RbST.new(@rst_file).to_html
     assert_equal html, File.read(@html_file)
   end
-  
+
   should "convert ReST to LaTeX" do
     latex = RbST.new(@rst_file).to_latex
     assert_equal latex, File.read(@latex_file)
   end
-  
+
   [:html, :latex].each do |f|
     should "accept options on #to_#{f}" do
       converter = RbST.new(@rst_file)
@@ -57,21 +57,21 @@ class TestRbST < Test::Unit::TestCase
       assert converter.send("to_#{f}", :raw_enabled)
     end
   end
-  
+
   should "recognize strip_comments option" do
     html_with_comment = RbST.convert(".. comment")
     assert_equal html_with_comment, %Q{<div class=\"document\">\n<!-- comment -->\n</div>\n}
     html_without_comment = RbST.convert(".. comment", 'strip-comments')
     assert_equal html_without_comment, %Q{<div class=\"document\">\n</div>\n}
   end
-  
+
   should "recognize cloak_email_addresses option" do
     html_with_uncloaked_email = RbST.convert("steve@mac.com")
     assert_equal %Q{<div class=\"document\">\n<p><a class=\"reference external\" href=\"mailto:steve&#64;mac.com\">steve&#64;mac.com</a></p>\n</div>\n}, html_with_uncloaked_email
     html_with_cloaked_email = RbST.convert("steve@mac.com", 'cloak-email-addresses')
     assert_equal %Q{<div class=\"document\">\n<p><a class=\"reference external\" href=\"mailto:steve&#37;&#52;&#48;mac&#46;com\">steve<span>&#64;</span>mac<span>&#46;</span>com</a></p>\n</div>\n}, html_with_cloaked_email
   end
-  
+
   should "recognize part option" do
     html_body = RbST.convert("hello world", :part => :html_body)
     assert_equal %Q{<div class=\"document\">\n<p>hello world</p>\n</div>\n}, html_body
@@ -83,5 +83,5 @@ class TestRbST < Test::Unit::TestCase
     output = RbST.convert("Hello ☃", :part => :fragment)
     assert_equal %Q{<p>Hello ☃</p>\n}, output
   end
-  
+
 end
