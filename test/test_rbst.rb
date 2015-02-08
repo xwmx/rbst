b53b7ec9200390aa1b1415b262960a7c9bf0522e
William Melody <hi@williammelody.com>
2015-02-08 13:00:48 -0800

    Use multiline method chaining and assert_equals in tests
    Multilining everything makes it easier to navigate and read, and it
    avoids having arguments far off to the side after long strings.
    
    Backslash / newline / period format for multiline chaining is used
    because it's the method that communicates the most about the fact that
    the chain is multilined. With this approach, it's immediately obvious
    that it's a chain no matter what line you look at.

--- test/test_rbst.rb
+++ test/test_rbst.rb
@@ -11,12 +11,17 @@ class TestRbST < Test::Unit::TestCase
         File.join(File.dirname(__FILE__), 'files', "test.#{f}")
       )
     end
-    @rst2parts_path = File.expand_path(File.join(File.dirname(__FILE__), '..', 'lib', 'rst2parts'))
+    @rst2parts_path = File.expand_path(
+      File.join(File.dirname(__FILE__), '..', 'lib', 'rst2parts')
+    )
   end
 
   should "call bare rest2parts when passed no options" do
     converter = RbST.new(@rst_file)
-    converter.expects(:execute).with("python #{@rst2parts_path}/rst2html.py").returns(true)
+    converter \
+      .expects(:execute) \
+      .with("python #{@rst2parts_path}/rst2html.py") \
+      .returns(true)
     assert converter.convert
   end
 
@@ -25,7 +30,10 @@ class TestRbST < Test::Unit::TestCase
     default_executables = RbST.executables
     RbST.executables = executables
     converter = RbST.new(@file)
-    converter.expects(:execute).with("python #{executables[:html]}").returns(true)
+    converter \
+      .expects(:execute) \
+      .with("python #{executables[:html]}") \
+      .returns(true)
     assert converter.to_html
     RbST.executables = default_executables
   end
@@ -42,46 +50,76 @@ class TestRbST < Test::Unit::TestCase
 
   should "convert ReST to html" do
     html = RbST.new(@rst_file).to_html
-    assert_equal html, File.read(@html_file)
+    assert_equal(
+      html,
+      File.read(@html_file)
+    )
   end
 
   should "convert ReST to LaTeX" do
     latex = RbST.new(@rst_file).to_latex
-    assert_equal latex, File.read(@latex_file)
+    assert_equal(
+      latex,
+      File.read(@latex_file)
+    )
   end
 
   [:html, :latex].each do |f|
     should "accept options on #to_#{f}" do
       converter = RbST.new(@rst_file)
-      converter.expects(:execute).with("python #{@rst2parts_path}/rst2#{f}.py --raw-enabled").returns(true)
+      converter \
+        .expects(:execute) \
+        .with("python #{@rst2parts_path}/rst2#{f}.py --raw-enabled") \
+        .returns(true)
       assert converter.send("to_#{f}", :raw_enabled)
     end
   end
 
   should "recognize strip_comments option" do
     html_with_comment = RbST.convert(".. comment")
-    assert_equal html_with_comment, %Q{<div class=\"document\">\n<!-- comment -->\n</div>\n}
+    assert_equal(
+      html_with_comment,
+      %Q{<div class=\"document\">\n<!-- comment -->\n</div>\n}
+    )
     html_without_comment = RbST.convert(".. comment", 'strip-comments')
-    assert_equal html_without_comment, %Q{<div class=\"document\">\n</div>\n}
+    assert_equal(
+      html_without_comment,
+      %Q{<div class=\"document\">\n</div>\n}
+    )
   end
 
   should "recognize cloak_email_addresses option" do
     html_with_uncloaked_email = RbST.convert("steve@mac.com")
-    assert_equal %Q{<div class=\"document\">\n<p><a class=\"reference external\" href=\"mailto:steve&#64;mac.com\">steve&#64;mac.com</a></p>\n</div>\n}, html_with_uncloaked_email
+    assert_equal(
+      %Q{<div class=\"document\">\n<p><a class=\"reference external\" href=\"mailto:steve&#64;mac.com\">steve&#64;mac.com</a></p>\n</div>\n},
+      html_with_uncloaked_email
+    )
     html_with_cloaked_email = RbST.convert("steve@mac.com", 'cloak-email-addresses')
-    assert_equal %Q{<div class=\"document\">\n<p><a class=\"reference external\" href=\"mailto:steve&#37;&#52;&#48;mac&#46;com\">steve<span>&#64;</span>mac<span>&#46;</span>com</a></p>\n</div>\n}, html_with_cloaked_email
+    assert_equal(
+      %Q{<div class=\"document\">\n<p><a class=\"reference external\" href=\"mailto:steve&#37;&#52;&#48;mac&#46;com\">steve<span>&#64;</span>mac<span>&#46;</span>com</a></p>\n</div>\n},
+      html_with_cloaked_email
+    )
   end
 
   should "recognize part option" do
     html_body = RbST.convert("hello world", :part => :html_body)
-    assert_equal %Q{<div class=\"document\">\n<p>hello world</p>\n</div>\n}, html_body
+    assert_equal(
+      %Q{<div class=\"document\">\n<p>hello world</p>\n</div>\n},
+      html_body
+    )
     fragment = RbST.convert("hello world", :part => :fragment)
-    assert_equal %Q{<p>hello world</p>\n}, fragment
+    assert_equal(
+      %Q{<p>hello world</p>\n},
+      fragment
+    )
   end
 
   should "convert unicode" do
     output = RbST.convert("Hello ☃", :part => :fragment)
-    assert_equal %Q{<p>Hello ☃</p>\n}, output
+    assert_equal(
+      %Q{<p>Hello ☃</p>\n},
+      output
+    )
   end
 
 end
