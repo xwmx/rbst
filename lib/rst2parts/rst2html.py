f4084a3c83ef69cf164ca6ca1a5684e7cf2e9de8
William Melody <hi@williammelody.com>
2020-04-16 17:54:12 -0700

    Reformat Python code with Black.
    https://github.com/psf/black

--- lib/rst2parts/rst2html.py
+++ lib/rst2parts/rst2html.py
@@ -2,7 +2,8 @@
 
 try:
     import locale
-    locale.setlocale(locale.LC_ALL, '')
+
+    locale.setlocale(locale.LC_ALL, "")
 except:
     pass
 
@@ -11,14 +12,16 @@ import sys
 from transform import transform
 from docutils.writers.html4css1 import Writer
 
-def main():
-    return transform(writer=Writer(), part='html_body')
 
-if __name__ == '__main__':
+def main():
+    return transform(writer=Writer(), part="html_body")
+
+
+if __name__ == "__main__":
     # Python 2 wants an encoded string for unicode, while Python 3 views an
     # encoded string as bytes and asks for a string. Solution via:
     # http://stackoverflow.com/a/24104423
     if sys.version_info[0] < 3:
-        UTF8Writer = codecs.getwriter('utf8')
+        UTF8Writer = codecs.getwriter("utf8")
         sys.stdout = UTF8Writer(sys.stdout)
     sys.stdout.write(main())
